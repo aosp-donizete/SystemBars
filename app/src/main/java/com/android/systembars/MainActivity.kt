79bc9fa098a6ff686429a5c85618879cad29b22c
Donizete Vida <donizetevida1998@outlook.com>
2023-05-16 18:49:51 -0300

    Initial project to assert navigation/status bar API's

---
+++ app/src/main/java/com/android/systembars/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.android.systembars
+
+import android.os.Build
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        window.applyCutout()
+
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            isAppearanceLightStatusBars = false
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            hide(WindowInsetsCompat.Type.systemBars())
+        }
+    }
+}
+
+fun Window.applyCutout() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        attributes.layoutInDisplayCutoutMode =
+            WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+    }
+}
