d76548354616a3976b3537dbe8e6b7d5fbeb19ea
Donizete Vida <donizetevida1998@outlook.com>
2023-06-15 10:48:06 -0300

    Control bars

--- app/src/main/java/com/android/systembars/MainActivity.kt
+++ app/src/main/java/com/android/systembars/MainActivity.kt
@@ -2,12 +2,15 @@ package com.android.systembars
 
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.view.postDelayed
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +22,39 @@ class MainActivity : AppCompatActivity() {
         //applied as XML resource
         //window.applyCutout()
 
-        WindowCompat.getInsetsController(window, window.decorView).apply {
-            isAppearanceLightStatusBars = false
-            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            hide(WindowInsetsCompat.Type.systemBars())
+        startWindowIssue(false)
+    }
+
+    private fun startWindowIssue(state: Boolean) {
+        Toast.makeText(this, "State: $state", Toast.LENGTH_SHORT).show()
+        if (state) {
+            addStatusBar()
+        } else {
+            removeStatusBar()
         }
+        window.decorView.postDelayed(5000) {
+            startWindowIssue(!state)
+        }
+    }
+
+    private inline fun onWindowInsetsControllerCompat(
+        block: WindowInsetsControllerCompat.() -> Unit
+    ) = WindowCompat.getInsetsController(window, window.decorView).apply(block)
+
+    fun removeStatusBar(v: View) = removeStatusBar()
+
+    private fun removeStatusBar() = onWindowInsetsControllerCompat {
+        isAppearanceLightStatusBars = false
+        systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        hide(WindowInsetsCompat.Type.systemBars())
+    }
+
+    fun addStatusBar(v: View) = addStatusBar()
+
+    private fun addStatusBar() = onWindowInsetsControllerCompat {
+        isAppearanceLightStatusBars = false
+        systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_DEFAULT
+        hide(0)
     }
 }
 
